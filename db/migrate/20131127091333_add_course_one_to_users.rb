1d40f06a3d598640aca8b370208abbd0c3431c8b
Claude K. Timothy <claudek@hotmail.com>
2013-12-13 10:59:36 +0300

    [Claude] - deleted files from git caches

--- db/migrate/20131127091333_add_course_one_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseOneToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :course_one, :string
-  end
-end
--- db/migrate/20131127091648_add_course_two_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseTwoToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :course_two, :string
-  end
-end
